601884a4eda776c41da9a6c3c0905cb19320374e
nguyenthanhnhan1006 <nhananh0964697276@gmail.com>
2023-12-21 23:00:12 +0700

    21/12 state

--- src/views/example/Mycomponent.js
+++ src/views/example/Mycomponent.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 class Mycomponent extends React.Component {
 
+    state = {
+        name: 'nhan',
+        tt: 'ngu lon'
+
+    }
+
+    /* 
+    JSX => return block 
+    <> </> = react.fragment
+   
+    */
+
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
+
+
         return (
-            <div>Hello my Component</div>
+            <>
+                <div className='first'>
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnchangeName(event)}
+                    />
+                    Hello my Component, my name is {this.state.name}
+                </div>
+                <div className='second'>
+                    nguoi dau ma {this.state.tt}
+                </div>
+            </>
         )
 
     }
