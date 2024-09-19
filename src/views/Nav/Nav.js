92d5cdc917efba230a08560a33cbf6c1e9432177
Your Name <you@example.com>
2024-09-19 20:55:06 +0700

    link,navLink

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Nav.scss";
+import { Link, NavLink } from "react-router-dom";
 
 class Nav extends React.Component {
   render() {
     return (
       <div className="topnav">
-        <a className="active" href="/">
+        <NavLink to="/" activeClassName="active" exact={true}>
           Home
-        </a>
-        <a href="/todo">Todos</a>
-        <a href="/about">About</a>
+        </NavLink>
+        <NavLink to="/todo" activeClassName="active">
+          Todos
+        </NavLink>
+        <NavLink to="/about" activeClassName="active">
+          About
+        </NavLink>
       </div>
     );
   }
