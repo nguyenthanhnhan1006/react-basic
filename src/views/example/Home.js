875b312a5d2683ee77758f799d9d0a56b215135b
Nguyễn Thanh Nhân <nhananh0964697276@gmail.com>
2025-03-13 20:23:24 +0700

    Redirect

--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 class Home extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.history.push("/todo");
+    }, 3000);
+  }
+  // HOC: higher order components
   render() {
+    console.log(" >>>>> checkprops", this.props);
     return (
       <div>
         <h1>Welcome to Home Page</h1>
@@ -11,4 +18,4 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+export default withRouter(Home);
