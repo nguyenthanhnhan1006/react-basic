34dcbfcac1d2149c1835d4dbb7f124504943d47d
Nguyễn Thanh Nhân <nhananh0964697276@gmail.com>
2025-03-14 20:23:53 +0700

    HOC

--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router";
+import Color from "../HOC/Color";
 class Home extends React.Component {
   componentDidMount() {
     setTimeout(() => {
@@ -18,4 +19,5 @@ class Home extends React.Component {
   }
 }
 
-export default withRouter(Home);
+// export default withRouter(Home);
+export default Color(Home);
